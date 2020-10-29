411d05145895a63803dab8d6f5c6ab4bb8b9c41d
Shawn <smm@Shawns-iMac.local>
2020-10-29 16:10:48 -0700

    auto generate id

--- src/App.js
+++ src/App.js
@@ -4,16 +4,15 @@ import endpoints from './endpoints.json';
 
 function App() {
   const [tasks, setTasks] = useState([]);
-  const [refresh, setRefresh] = useState(true);
+  const [refreshList, setRefreshList] = useState(true);
 
   useEffect(() => {
-    if (refresh) {
+    if (refreshList) {
       fetch(endpoints.getTasks).then(response => response.text()).then(result => {
         const taskItems = JSON.parse(result).body.Items;
-        console.log(taskItems);
         setTasks(taskItems);
       }).catch(error => console.error(error));
-      setRefresh(false);
+      setRefreshList(false);
     }
   });
 
@@ -21,7 +20,7 @@ function App() {
     var myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
     // using built in JSON utility package turn object to string and store in a variable
-    const id = Number.parseInt(document.getElementById('id').value);
+    const id = Date.now(); //Number.parseInt(document.getElementById('id').value);
     const title = document.getElementById('title').value;
     var raw = JSON.stringify(
       { "id":id,
@@ -38,18 +37,19 @@ function App() {
     };
     // make API call with parameters and use promises to get response
     fetch(endpoints.createTask, requestOptions).then(response => {
-      setRefresh(true);
+      setRefreshList(true);
     }).catch(error => console.log('error', error));
   };
 
   return (
     <div className="App">
       <table>
-        {tasks.map(task => <tr><td><input type="checkbox"/></td><td>{task.Title}</td></tr>)}
+        <tbody>
+          {tasks.map(task => <tr key={task.ID}><td><input type="checkbox"/></td><td>{task.Title}</td></tr>)}
+        </tbody>
       </table>
       <form>
-        ID: <input type="text" name="id" id="id" />
-        Title: <input type="text" name="title" id="title" />
+        <input type="text" name="title" id="title" />
         <button type="button" onClick={createTask}>Create Task</button>
       </form>
     </div>
